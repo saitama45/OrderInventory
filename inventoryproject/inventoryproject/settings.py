8a2ab30569b6a678afd64e3f3ef5431448ab0649
saitama45 <genermagbanua45@gmail.com>
2022-02-14 23:07:02 +0800

    completed interface setup

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -130,6 +130,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = (BASE_DIR/"asset/")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
