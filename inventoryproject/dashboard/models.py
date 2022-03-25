b8b92222b499430ecebff6d79c3ac307e0fe4ea8
Nin Alamo <nino.alamo@opteonsolutions.com>
2022-03-25 18:52:29 +0800

    Database relationship part 1

--- inventoryproject/dashboard/models.py
+++ inventoryproject/dashboard/models.py
@@ -1,4 +1,7 @@
+from asyncio.windows_events import NULL
+from tabnanny import verbose
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY = (
@@ -14,5 +17,20 @@ class Product(models.Model):
     category = models.CharField(max_length=25, choices=CATEGORY, null=True)
     quantity = models.PositiveIntegerField(null=True)
 
+    class Meta:
+        verbose_name_plural = 'Product'
+
     def __str__(self):
         return f'{self.name} - {self.quantity}'
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Order'
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
